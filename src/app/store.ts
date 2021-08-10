0e353a178c015c457be14ff07422a6901cf3defe
slavikyarkin <slavikyarkin@gmail.com>
2021-08-10 21:20:27 +0300

    add signIn

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import signInReducer from '../features/signIn/signInSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    signIn: signInReducer,
   },
 });
 
---
+++ src/features/signIn/signInSlice.ts
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+
+export interface SignInState {
+  email: string;
+  password: string;
+  status: 'idle' | 'loading' | 'failed';
+}
+
+const initialState: SignInState = {
+  email: '',
+  password: '',
+  status: 'idle',
+};
+
+export const incrementAsync = createAsyncThunk(
+  'signIn/fetchSignIn',
+  async (amount: number) => {
+    const response = await fetchCount(amount);
+    return response.data;
+  }
+);
+
+
+export const signInSlice = createSlice({
+  name: 'signIn',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+  },
+});
+
+export const {} = signInSlice.actions;
+
+export const selectEmail = (state: RootState) => state.signIn.email;
+export const selectPassword = (state: RootState) => state.signIn.password;
+
+export default signInSlice.reducer;
